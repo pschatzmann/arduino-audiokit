92d9dd9f5bd2ea7caf02de43d3e657ddacab100f
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-04-08 14:55:13 +0200

    Correct output example

--- examples/output/output.ino
+++ examples/output/output.ino
@@ -8,20 +8,28 @@
  * 
  */
 #include "AudioKitHAL.h"
+#include "SineWaveGenerator.h"
 
 
 AudioKit kit;
+SineWaveGenerator wave;
+const int BUFFER_SIZE = 1024;
+uint8_t buffer[BUFFER_SIZE];
 
 void setup() {
   LOGLEVEL_AUDIOKIT = AudioKitInfo; 
   Serial.begin(115200);
   // open in write mode
-  auto cfg = kit.defaultConfig();
+  auto cfg = kit.defaultConfig(AudioOutput);
   kit.begin(cfg);
+
+  // 1000 hz
+  wave.setFrequency(1000);
+  wave.setSampleRate(cfg.sampleRate());
 
 }
 
 void loop() {
-  AudioKit::actionHeadphoneDetection();
-  delay(10);
+  size_t l = wave.read(buffer, BUFFER_SIZE);
+  kit.write(buffer, l);
 }
