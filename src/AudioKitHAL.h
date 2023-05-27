bcbb253b2c107c77047bd15c73ed6423383b97b4
Phil Schatzmann <phil.schatzmann@gmail.com>
2023-05-27 09:59:16 +0200

    Prepare to support AUDIO_DRIVER_ADC

--- src/AudioKitHAL.h
+++ src/AudioKitHAL.h
@@ -24,51 +24,7 @@
 #include "audio_hal/audiokit_board_select.h"
 #include "audio_hal/audiokit_logger.h"
 #include "audio_hal/driver_pins.h"
-
-// flag to indicate that the config supports pins
-#define AUDIOKIT_V1
-
-#if AUDIOKIT_SETUP_SD
-#include "SPI.h"
-#ifdef ESP32
-#include "audio_hal/audio_system.h"
-#include "audio_hal/audio_type_def.h"
-#include "audio_hal/audio_version.h"
-#include "driver/i2s.h"
-#if !defined(CONFIG_IDF_TARGET_ESP32S3) && !defined(CONFIG_IDF_TARGET_ESP32S2)                     \
-    && !defined(CONFIG_IDF_TARGET_ESP32C3)
-SPIClass SPI_VSPI(VSPI);
-#endif
-#endif
-#endif
-
-// Support for old IDF versions
-#if AUDIOKIT_ESP32_I2S
-#if ESP_IDF_VERSION_MAJOR < 4 && !defined(I2S_COMM_FORMAT_STAND_I2S)
-#define I2S_COMM_FORMAT_STAND_I2S (I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB)
-#define I2S_COMM_FORMAT_STAND_MSB (I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_LSB)
-#define I2S_COMM_FORMAT_STAND_PCM_LONG (I2S_COMM_FORMAT_PCM | I2S_COMM_FORMAT_PCM_LONG)
-#define I2S_COMM_FORMAT_STAND_PCM_SHORT (I2S_COMM_FORMAT_PCM | I2S_COMM_FORMAT_PCM_SHORT)
-typedef int eps32_i2s_audio_sample_rate_type;
-#else
-typedef uint32_t eps32_i2s_audio_sample_rate_type;
-#endif
-#endif
-
-// Define LED_BUILTIN
-#if defined(GREEN_LED_GPIO)
-#undef LED_BUILTIN
-#define LED_BUILTIN GREEN_LED_GPIO
-#endif
-
-// When we do not have Arduino we need to provide the following implementations
-#ifndef ARDUINO
-#ifdef AUDIOKIT_FREE_RTOS
-void yield() { taskYIELD(); }
-#else
-void yield() { }
-#endif
-#endif
+#include "audio_hal/common_defines.h"
 
 namespace audiokit {
 
--- src/audio_driver/es8156/es8156.c
+++ src/audio_driver/es8156/es8156.c
@@ -33,6 +33,7 @@
 
 
 #define ES8156_ADDR         0x10
+#define BOARD_PA_GAIN               (0) /* Power amplifier gain defined by board (dB) */
 
 //static const char *TAG = "DRV8156";
 static bool codec_init_flag = 0;
