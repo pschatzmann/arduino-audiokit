796a4068e49fdc89f988c7a312c07dec23d039db
Phil Schatzmann <phil.schatzmann@gmail.com>
2023-09-25 07:13:46 +0200

    Compile error board 7

--- src/audio_board/ai_thinker_es8388_7.h
+++ src/audio_board/ai_thinker_es8388_7.h
@@ -73,7 +73,7 @@
 #define RESET_CODEC                -1
 #define RESET_BOARD                -1
 
-extern audio_hal_func_t AUDIO_CODEC_ES8388_DEFAULT_HANDLE;
+// extern audio_hal_func_t AUDIO_CODEC_ES8388_DEFAULT_HANDLE;
 
 #define AUDIO_CODEC_DEFAULT_CONFIG(){                   \
         .adc_input  = AUDIO_HAL_ADC_INPUT_LINE1,        \
